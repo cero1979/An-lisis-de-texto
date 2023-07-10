0d8f2434ceb0b15992c700332df17e7b10997c68
Carlos Ramirez <113059505+cero1979@users.noreply.github.com>
2023-07-10 07:28:10 -0500

    Update GrafoF.m

--- GrafoF.m
+++ GrafoF.m
@@ -2,16 +2,15 @@ datos = readmatrix('conexionF.csv');
 nodos = datos(:, 1:2);
 pesos = datos(:, 3);
 
-% Crear una tabla con los datos
+
 tabla = table(nodos, pesos, 'VariableNames', {'Nodes', 'Weight'});
 
-% Ordenar los nodos en cada fila de forma ascendente
+
 tabla.Nodes = sort(tabla.Nodes, 2);
 
-% Eliminar las filas duplicadas manteniendo solo la primera aparición
+
 tabla = unique(tabla, 'rows', 'stable');
 
-% Construir el grafo y graficarlo
 figure;
 grafo = graph(tabla.Nodes(:, 1), tabla.Nodes(:, 2), tabla.Weight, 'OmitSelfLoops');
 plot(grafo, 'EdgeLabel', grafo.Edges.Weight);
