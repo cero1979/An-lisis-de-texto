9f4e54b65494f5fea8dbc1080bf3a91519e5b74b
Carlos Ramirez <113059505+cero1979@users.noreply.github.com>
2023-07-10 07:27:49 -0500

    Update ClasificaNodos.m

--- ClasificaNodos.m
+++ ClasificaNodos.m
@@ -1,7 +1,6 @@
-% Paso 1: Leer archivo CSV como matriz numérica
+
 datos = readmatrix('metricas_nodos.csv', 'HeaderLines', 1);
 
-% Paso 2: Calcular cuartiles y categorías
 cuartiles = zeros(3, 6); % Matriz para almacenar los cuartiles
 for j = 1:6
     cuartiles(:, j) = prctile(datos(:, j), [25, 50, 75])';
@@ -10,7 +9,7 @@ end
 N = size(datos, 1);  % Número de nodos
 categorias = zeros(N, 18);
 
-% Paso 3: Asignar categorías a cada nodo y métrica
+
 for i = 1:N
     for j = 1:6
         metrica = datos(i, j);
@@ -27,10 +26,8 @@ for i = 1:N
     end
 end
 
-% Paso 4: Combinar nodos y categorías
 datos_con_categorias = [datos, categorias];
 
-% Paso 5: Guardar resultados en un archivo CSV
 encabezados = ['Nodo', 'Eigenvector Centrality', 'Betweenness Centrality', 'Closeness Centrality', 'Degree Centrality', 'Clustering Coefficient', 'Exccentricity', 'Alto (EC)', 'Medio (EC)', 'Bajo (EC)', 'Alto (BC)', 'Medio (BC)', 'Bajo (BC)', 'Alto (CC)', 'Medio (CC)', 'Bajo (CC)', 'Alto (DC)', 'Medio (DC)', 'Bajo (DC)', 'Alto (CCo)', 'Medio (CCo)', 'Bajo (CCo)', 'Alto (Ex)', 'Medio (Ex)', 'Bajo (Ex)'];
 
 resultado_csv = ['resultados.csv'];
